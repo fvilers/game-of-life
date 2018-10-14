f010eb637cad55e2c94cb8f64f5421c1ed429b21
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-14 08:52:23 +0200

    Add the createGrid function

--- src/sketch.js
+++ src/sketch.js
@@ -1,3 +1,13 @@
-function setup() {}
+let grid;
+
+function createGrid(rows, cols) {
+  return Array(rows)
+    .fill(0)
+    .map(x => Array(cols || rows).fill(0));
+}
+
+function setup() {
+  grid = createGrid(10);
+}
 
 function draw() {}

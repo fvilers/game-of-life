c1678188ef5f9b1ebb61eb77ea93a4db3cbf370d
Fabian Vilers <fabian.vilers@dev-one.com>
2018-10-14 09:49:36 +0200

    Compute the next state of the grid

--- src/sketch.js
+++ src/sketch.js
@@ -1,5 +1,5 @@
-const ROWS = 5;
-const COLS = 10;
+const ROWS = 50;
+const COLS = 100;
 const SCALE = 10;
 
 let grid;
@@ -10,26 +10,65 @@ function createGrid(rows, cols) {
     .map(x => Array(cols || rows).fill(0));
 }
 
+function countNeighbors(array, x, y) {
+  // Count only neighbors (not the cell itself)
+  let neighbors = -array[y][x];
+
+  for (let row = -1; row <= 1; row++) {
+    for (let col = -1; col <= 1; col++) {
+      // Ensure no out of bounds as edges are stitched together
+      neighbors += array[(y + row + ROWS) % ROWS][(x + col + COLS) % COLS];
+    }
+  }
+
+  return neighbors;
+}
+
+function computeNextGrid(previous) {
+  const next = createGrid(ROWS, COLS);
+
+  // Determine next state for each cells
+  for (let row = 0; row < previous.length; row++) {
+    for (let col = 0; col < previous[row].length; col++) {
+      const cell = previous[row][col];
+      const neighbors = countNeighbors(previous, col, row);
+
+      if (!cell && neighbors === 3) {
+        next[row][col] = 1;
+      } else if (cell && (neighbors < 2 || neighbors > 3)) {
+        next[row][col] = 0;
+      } else {
+        next[row][col] = cell;
+      }
+    }
+  }
+
+  return next;
+}
+
 function setup() {
   // Create the canvas (add 1 for the stroke line)
   createCanvas(COLS * SCALE + 1, ROWS * SCALE + 1);
 
   // Create the grid and randomize content
   grid = createGrid(ROWS, COLS);
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      grid[i][j] = floor(random(2));
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      grid[row][col] = floor(random(2));
     }
   }
 }
 
 function draw() {
   // Draw each cell: black for living cells, white for dead ones
-  for (let i = 0; i < grid.length; i++) {
-    for (let j = 0; j < grid[i].length; j++) {
-      fill(!grid[i][j] * 255);
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[row].length; col++) {
+      fill(!grid[row][col] * 255);
       stroke(128);
-      rect(j * SCALE, i * SCALE, SCALE, SCALE);
+      rect(col * SCALE, row * SCALE, SCALE, SCALE);
     }
   }
+
+  // Compute next grid state
+  grid = computeNextGrid(grid);
 }
